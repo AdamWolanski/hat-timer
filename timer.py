e7567d08f5e28d2968e378625d56f7825638fdb4
AdamWolanski <adam.wolanski@tieto.com>
2015-11-27 11:14:53 +0100

    timer fix

--- timer.py
+++ timer.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python
 
+import sys
+
 class Time:
     def __init__(self, argv):
         self.args = argv
         self.time = 0
         self.hours = 0
         self.minutes = 0
+        self.flag = 1
         self.timeSet()
         self.timePrint()
     def timePrint(self):
+        if self.flag == 1:
+            print "Masz w zapasie: "
+        else:
+            print "Brakuje ci: "
         print "{}:{}".format(self.hours, self.minutes)
     def timeSet(self):
         for t in self.args:
             self.time += float(t)
         self.time *= (60*8)
+        if self.time < 0:
+            self.flag = 0
+        else:
+            self.flag = 1
         self.hours = int(self.time/60)
         self.minutes = int(self.hours * 60 - self.time)
+        self.hours = abs(self.hours)
+        self.minutes = abs(self.minutes)
 
 def main(argv):
     t = Time(argv)
